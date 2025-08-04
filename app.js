3f469383f646f571176f68c0e2b616720ae391b5
AGBaez094 <vi10267@innovaccion.mx>
2025-08-04 00:08:00 -0600

    declarar funcion para agregar nombres y corregir error de llamada en html

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigosIngresados = 0;
+let nombreIngresado = "";
+
+
+function agregarAmigo(){
+    nombreIngresado= document.getElementById("amigo").value;
+
+    if (nombreIngresado === "" || nombreIngresado.includes(" ")){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(nombreIngresado);
+        console.log(amigos);
+        nombreIngresado = "";
+        amigosIngresados ++;
+        
+    }
+    
+}
+
+function sortearAmigo(){
+
+}
