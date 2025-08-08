67b08bf0b1f84806f576b26eea64757e6c331ed1
AGBaez094 <vi10267@innovaccion.mx>
2025-08-08 00:26:21 -0600

    Modificar en HTML y CSS para que el campo de llenado solo admita mayusculas y minusculas, sin que este pierda los estilos y funciones asignadas

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let amigos = [];
 let amigosIngresados = 0;
 let nombreIngresado = "";
 
-
-
 function agregarAmigo(){
+    
+    //Aqui se ingresan los nombres uno por uno
     nombreIngresado= document.getElementById("amigo").value;
     const miLista = document.getElementById("listaAmigos");
 
@@ -22,11 +22,15 @@ function agregarAmigo(){
         //agregar un elemento para agregar elementos por lenght 0,1,2,3 del arreglo
         miLista.innerHTML = "";
         amigos.forEach(elemento => {
+
         const nuevoItem = document.createElement("li");
+
         nuevoItem.textContent = elemento;
         miLista.appendChild(nuevoItem);
         
     });
+
+
 }
 
 function limpiarCampo(){
@@ -34,10 +38,16 @@ function limpiarCampo(){
 }
 
 function sortearAmigo(){
-    
+
     const nombresSorteados = [...amigos];
+    const amigoSorteado = document.getElementById("resultado");
+    const amigoSecreto = document.createElement("li");
+
     nombresSorteados.sort(() => Math.random() - 0.5);
     const primerSorteado = nombresSorteados[0];
-    console.log(`El amigo secreto es: ${primerSorteado}`);
+    amigoSorteado.innerHTML = "";
+
+    amigoSecreto.textContent = (`El amigo secreto es: ${primerSorteado}`);
+    amigoSorteado.appendChild(amigoSecreto);
 
 }
